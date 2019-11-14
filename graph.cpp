72fa492ff61d186ee174498dd2bba39535bb4cab
AgentJ08 <arpit.maheshwari08041999@gmail.com>
2019-11-14 23:58:06 +0530

    initial commit

---
+++ graph.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+class date
+{
+public:
+	int dd;
+	int mm;
+	int yyyy;
+}
+
+class chit
+{
+public:
+	
+}
+
+class profile
+{
+public:
+	string fname;
+	string sname;
+	string uname;
+	date bday;
+	date joined;
+	string bio;
+	//chitters list
+	//liked list
+	//rechitted list
+	//following
+	//followers
+}
+
+
